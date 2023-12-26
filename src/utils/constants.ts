7a56ed38abd7fcb1cb5eddf889038329c52f8e28
Kirill13311 <62827666+KirillEL@users.noreply.github.com>
2023-12-26 13:05:25 +0700

    adaptive for server and host

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL='http://localhost:1010/api'
+export const API_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVER_API_URL : process.env.NEXT_PUBLIC_LOCAL_API_URL;
